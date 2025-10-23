6547d04d2844d2e2179d96abaab4d131e7b5b6f1
spicypunch <miniguni66@naver.com>
2025-10-24 01:10:22 +0900

    Commit

--- brick_breaker/lib/src/components/ball.dart
+++ brick_breaker/lib/src/components/ball.dart
@@ -1,8 +1,14 @@
 import 'dart:ui';
 
+import 'package:brick_breaker/src/brick_breaker.dart';
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 
-class Ball extends CircleComponent {
+import 'components.dart';
+
+class Ball extends CircleComponent
+    with CollisionCallbacks, HasGameReference<BrickBreaker> {
   Ball({
     required this.velocity,
     required super.position,
@@ -13,6 +19,7 @@ class Ball extends CircleComponent {
          paint: Paint()
            ..color = const Color(0xff1e6091)
            ..style = PaintingStyle.fill,
+         children: [CircleHitbox()],
        );
 
   final Vector2 velocity;
@@ -21,5 +28,31 @@ class Ball extends CircleComponent {
   void update(double dt) {
     super.update(dt);
     position += velocity * dt;
+  }
+
+  @override
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is PlayArea) {
+      if (intersectionPoints.first.y <= 0) {
+        velocity.y = -velocity.y;
+      } else if (intersectionPoints.first.x <= 0) {
+        velocity.x = -velocity.x;
+      } else if (intersectionPoints.first.x >= game.width) {
+        velocity.x = -velocity.x;
+      } else if (intersectionPoints.first.y >= game.height) {
+        add(RemoveEffect(delay: 0.35));
+      }
+    } else if (other is Bat) {
+      velocity.y = -velocity.y;
+      velocity.x =
+          velocity.x +
+          (position.x - other.position.x) / other.size.x * game.width * 0.3;
+    } else {
+      print('Collision with $other');
+    }
   }
 }
